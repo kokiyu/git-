1b1e04eb99f2bcc3072a01b7300b4f5551fcb0f3
kokiyu <kokiyu1979@sina.com>
2017-02-28 11:22:45 +0800

    完成首页
    已完成首页

--- script/main.js
+++ script/main.js
@@ -1,7 +1,43 @@
 
 var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
+	el: '#all',
+	data: {
+		seen: false,
+		seenQQ:'hidden',
+		seenQQimg:'image/QQ.png',
+		seenWechat:'hidden',
+		seenWechatImg:'image/wechat.png',
+		seenWeibo:'hidden',
+		seenWeiboImg:'image/weibo.png'
+	},
+	methods: {
+		// var that =this;
+		openQQway:function(){
+			this.seenQQ = 'visible';
+			this.seenQQimg = 'image/QQ_beSelected.png';
+		},
+		closeQQWay:function(){
+			this.seenQQ = 'hidden';
+			this.seenQQimg = 'image/QQ.png';
+
+		},
+		openWechatWay:function(){
+			this.seenWechat = 'visible';
+			this.seenWechatImg = 'image/wechat__beSelected.png';
+		},
+		closeWechatWay:function(){
+			this.seenWechat = 'hidden';
+			this.seenWechatImg = 'image/wechat.png';
+		},
+		openWeiboWay:function(){
+			this.seenWeibo = 'visible';
+			this.seenWeiboImg = 'image/weibo__beSelected.png';
+
+		},
+		closeWeiboWay:function(){
+			this.seenWeibo = 'hidden';
+			this.seenWeiboImg = 'image/weibo.png';
+
+		}
+	}
 })
